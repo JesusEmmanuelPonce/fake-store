55eaa738f084e6e065b9343b8b97a19494ffb4e1
JesusEmmanuelPonce <j.emmanuel.pb@hotmail.com>
2023-09-03 11:03:50 -0600

    feat: added input to filters

--- src/pages/Store/Store.jsx
+++ src/pages/Store/Store.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import CustomSkeleton from '../../components/CustomSkeleton';
+import { useState } from 'react';
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 
 import Product from '../../components/Product/Product';
+import CustomSkeleton from '../../components/CustomSkeleton';
 import "./styles.scss";
 
 const Store = ({
@@ -10,9 +11,50 @@ const Store = ({
   }
 }) => {
 
+  const [order, setOrder] = useState('');
+  const [quantity, setQuantity] = useState('');
+
+  const handleChange = ({ target: { value } }) => {
+    setQuantity(value);
+  };
+
+  const handleOrderChange = ({ target: { value } }) => {
+    setOrder(value);
+  };
+
   return (
     <section className="container store">
-      <h1>Store</h1>
+      <section className='store__filters d-flex justify-content-end'>
+        <FormControl variant="standard" sx={{ minWidth: 120 }}>
+          <InputLabel id="quantity-label">Quantity</InputLabel>
+          <Select
+            labelId="quantity-label"
+            id="quantity"
+            value={quantity}
+            label="Quantity"
+            onChange={handleChange}
+          >
+            <MenuItem value={10}>10</MenuItem>
+            <MenuItem value={20}>20</MenuItem>
+            <MenuItem value={30}>30</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl variant="standard" sx={{ minWidth: 120 }}>
+          <InputLabel id="quantity-label">Order</InputLabel>
+          <Select
+            labelId="quantity-label"
+            id="quantity"
+            value={order}
+            label="Order"
+            onChange={handleOrderChange}
+          >
+            <MenuItem value="asc">Asc</MenuItem>
+            <MenuItem value="desc">Desc</MenuItem>
+          </Select>
+        </FormControl>
+      </section>
+
       <div className="row">
         {!products.length < 1 ?
           products.map(product => (
